960221a3398f8e8c75bb920b3654395707179417
yawar2019 <yawarali17@gmail.com>
2021-05-26 23:20:07 +0530

    committed

---
+++ AdoDotnetExample/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using AdoDotnetExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoDotnetExample.Controllers
+{
+    public class DefaultController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View(db.getEmployees());
+        }
+    }
+}
--- AdoDotnetExample/Models/EmployeeContext.cs
+++ AdoDotnetExample/Models/EmployeeContext.cs
@@ -9,10 +9,25 @@ namespace AdoDotnetExample.Models
 {
     public class EmployeeContext
     {
-        public List<EmployeeModel> getEmployees() {
-            List<EmployeeModel> listObj = new List<EmployeeModel>();
-            
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
 
+        public List<EmployeeModel> getEmployees() {
+
+            List<EmployeeModel> listObj = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("usp_getEmployeeDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel obj = new EmployeeModel();
+                obj.EmpId =Convert.ToInt32(dr[0]);
+                obj.EmpName = Convert.ToString(dr[1]);
+                obj.EmpSalary = Convert.ToInt32(dr[2]);
+                listObj.Add(obj);
+            }
             return listObj;
         }
     }
