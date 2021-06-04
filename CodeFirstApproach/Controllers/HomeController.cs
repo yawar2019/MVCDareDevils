2e59af39bfec5a54291896765260d836190a0068
yawar2019 <yawarali17@gmail.com>
2021-06-04 23:24:16 +0530

    committed

---
+++ CodeFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using CodeFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//insert query 
+
+            int i = db.SaveChanges();
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int ? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State=EntityState.Modified;//Update query 
+
+            int i = db.SaveChanges();
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
